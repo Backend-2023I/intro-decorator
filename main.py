864a6bce683cb742a8503c05a52b57d7a44477e4
JavohirJalilov <javohirjaliloff@gmail.com>
2023-09-22 15:09:38 +0500

    read json

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+import json
+
 def decorator(func):
     def wrapper(path:str):
-        pass
+        name, suffix = path.split('.')
+        if suffix == "json":
+            return func(path)
+        else:
+            return "not json file"
 
     return wrapper
 
 @decorator
-def redJson(path:str):
-    pass
+def readJson(path:str):
+    f = open(path)
+    data = json.load(f)
+    return data
+    
+print(readJson('data.csv'))
